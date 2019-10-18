7c4f02c541eee28381644e0a02cb2c2207c56f2c
SilviaSto <stoianova.silvia@yahoo.com>
2019-10-18 11:33:43 +0000

    add controller.js

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,13 @@
+sap.ui.define(					// define module(AMD)
+	[
+		"sap/ui/core/mvc/Controller",   //dependancies
+		"sap/m/MessageToast"
+	], 
+	function (Controller, MessageToast) {        // we put the names of the used dependancies as a callback functions
+		Controller.extend("opensap.myapp.controller.App", {  //custom object
+			onShowHello: function() {						
+				MessageToast.show("Text");	
+			}
+		});
+	}
+	);
