3635e95c1ecb2dde1ced24741d3f3eed8b8fa34a
SilviaSto <stoianova.silvia@yahoo.com>
2019-10-21 08:40:17 +0000

    add internalization model

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -6,7 +6,13 @@ sap.ui.define(					// define module(AMD)
 	function (Controller, MessageToast) {        // we put the names of the used dependancies as a callback functions
 		Controller.extend("opensap.myapp.controller.App", {  //custom object
 			onShowHello: function() {						
-				MessageToast.show("Text");	
+							// read msg from i18n model
+			var oBundle = this.getView().getModel("i18n").getResourceBundle();  //we get the msg bundle
+			var sRecipient = this.getView().getModel("textPanel").getProperty("/recipient/name");  // we want to display the recipient
+			var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
+			// show message
+			MessageToast.show(sMsg);
 			}
 		});
 	}
